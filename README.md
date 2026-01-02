748944ef7a158ea8de7af309e218253e7ee84d2b
fxyz <andrey.lischina@gmail.com>
2026-01-02 15:18:39 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ../
+make
+sudo make install
---
+++ indi_mycustomdriver.cpp
@@ -0,0 +1,181 @@
+#include <cstring>
+
+#include "libindi/indicom.h"
+
+#include "config.h"
+#include "indi_mycustomdriver.h"
+
+// int main() {
+//     printf("Hello\n");
+//     return 0;
+// }
+
+// We declare an auto pointer to DummyFocuser.
+static std::unique_ptr<MyFocuser> mydriver(new MyFocuser());
+
+MyFocuser::MyFocuser()
+{
+    setVersion(CDRIVER_VERSION_MAJOR, CDRIVER_VERSION_MINOR);
+
+    // Here we tell the base Focuser class what types of connections we can support
+    setSupportedConnections(CONNECTION_SERIAL | CONNECTION_TCP);
+
+    // And here we tell the base class about our focuser's capabilities.
+    SetCapability(FOCUSER_CAN_ABS_MOVE | FOCUSER_CAN_REL_MOVE | FOCUSER_CAN_ABORT);
+}
+
+const char *MyFocuser::getDefaultName()
+{
+    return "Dummy Focuser";
+}
+
+bool MyFocuser::initProperties()
+{
+    // initialize the parent's properties first
+    INDI::Focuser::initProperties();
+
+    // TODO: Add any custom properties you need here.
+
+    addAuxControls();
+
+    return true;
+}
+
+void MyFocuser::ISGetProperties(const char *dev)
+{
+    INDI::Focuser::ISGetProperties(dev);
+
+    // TODO: Call define* for any custom properties.
+}
+
+bool MyFocuser::updateProperties()
+{
+    INDI::Focuser::updateProperties();
+
+    if (isConnected())
+    {
+        // TODO: Call define* for any custom properties only visible when connected.
+    }
+    else
+    {
+        // TODO: Call deleteProperty for any custom properties only visible when connected.
+    }
+
+    return true;
+}
+
+bool MyFocuser::ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n)
+{
+    // Make sure it is for us.
+    if (dev != nullptr && strcmp(dev, getDeviceName()) == 0)
+    {
+        // TODO: Check to see if this is for any of my custom Number properties.
+    }
+
+    // Nobody has claimed this, so let the parent handle it
+    return INDI::Focuser::ISNewNumber(dev, name, values, names, n);
+}
+
+bool MyFocuser::ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n)
+{
+    // Make sure it is for us.
+    if (dev != nullptr && strcmp(dev, getDeviceName()) == 0)
+    {
+        // TODO: Check to see if this is for any of my custom Switch properties.
+    }
+
+    // Nobody has claimed this, so let the parent handle it
+    return INDI::Focuser::ISNewSwitch(dev, name, states, names, n);
+}
+
+bool MyFocuser::ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n)
+{
+    // Make sure it is for us.
+    if (dev != nullptr && strcmp(dev, getDeviceName()) == 0)
+    {
+        // TODO: Check to see if this is for any of my custom Text properties.
+    }
+
+    // Nobody has claimed this, so let the parent handle it
+    return INDI::Focuser::ISNewText(dev, name, texts, names, n);
+}
+
+bool MyFocuser::ISSnoopDevice(XMLEle *root)
+{
+    // TODO: Check to see if this is for any of my custom Snoops. Fo shizzle.
+
+    return INDI::Focuser::ISSnoopDevice(root);
+}
+
+bool MyFocuser::saveConfigItems(FILE *fp)
+{
+    INDI::Focuser::saveConfigItems(fp);
+
+    // TODO: Call IUSaveConfig* for any custom properties I want to save.
+
+    return true;
+}
+
+bool MyFocuser::Handshake()
+{
+    if (isSimulation())
+    {
+        LOGF_INFO("Connected successfuly to simulated %s.", getDeviceName());
+        return true;
+    }
+
+    // NOTE: PortFD is set by the base class.
+
+    // TODO: Any initial communciation needed with our focuser, we have an active
+    // connection.
+
+    return true;
+}
+
+void MyFocuser::TimerHit()
+{
+    if (!isConnected())
+        return;
+
+    // TODO: Poll your device if necessary. Otherwise delete this method and it's
+    // declaration in the header file.
+
+    LOG_INFO("timer hit");
+
+    // If you don't call SetTimer, we'll never get called again, until we disconnect
+    // and reconnect.
+    SetTimer(POLLMS);
+}
+
+IPState MyFocuser::MoveFocuser(FocusDirection dir, int speed, uint16_t duration)
+{
+    // NOTE: This is needed if we don't specify FOCUSER_CAN_ABS_MOVE
+    // TODO: Actual code to move the focuser. You can use IEAddTimer to do a
+    // callback after "duration" to stop your focuser.
+    LOGF_INFO("MoveFocuser: %d %d %d", dir, speed, duration);
+    return IPS_OK;
+}
+
+IPState MyFocuser::MoveAbsFocuser(uint32_t targetTicks)
+{
+    // NOTE: This is needed if we do specify FOCUSER_CAN_ABS_MOVE
+    // TODO: Actual code to move the focuser.
+    LOGF_INFO("MoveAbsFocuser: %d", targetTicks);
+    return IPS_OK;
+}
+
+IPState MyFocuser::MoveRelFocuser(FocusDirection dir, uint32_t ticks)
+{
+    // NOTE: This is needed if we do specify FOCUSER_CAN_REL_MOVE
+    // TODO: Actual code to move the focuser.
+    LOGF_INFO("MoveRelFocuser: %d %d", dir, ticks);
+    return IPS_OK;
+}
+
+bool MyFocuser::AbortFocuser()
+{
+    // NOTE: This is needed if we do specify FOCUSER_CAN_ABORT
+    // TODO: Actual code to stop the focuser.
+    LOG_INFO("AbortFocuser");
+    return true;
+}
